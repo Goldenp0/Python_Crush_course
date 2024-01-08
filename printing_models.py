fc975c7ce2d82278d97589a7c2b0794577641b81
Goldenp0 <preciousatuonwu25@gmail.com>
2024-01-08 23:24:01 +0100

    funtion

---
+++ printing_models.py
@@ -0,0 +1,10 @@
+# printing_models.py
+from printing_functions import print_models, show_completed_models
+
+# Example usage
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
